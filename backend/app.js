7e40afd5e2d3a1bc85529665654f71af69622d87
Bineth <binethellepola@gmail.com>
2026-02-09 20:07:37 +0530

    App.js and env file was created

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+// Import packages
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error.message);
+  });
+
+// Testing 
+app.get("/", (req, res) => {
+  res.send("MERN Backend is Running...");
+});
