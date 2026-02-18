aca09d9a2fc915b179fac3c5a2da6e745f0b7a7b
Bineth ellepola <binethellepola@gmail.com>
2026-02-18 19:02:02 +0530

    ..

--- backend/Route/UserRoute.js
+++ backend/Route/UserRoute.js
@@ -4,7 +4,6 @@ const userController = require("../Controller/UserController");
 const auth = require("../middleware/authMiddleware");
 const authorize = require("../middleware/roleMiddleware");
 
-/* Only Admin */
 router.get("/", auth, userController.getAllUsers);
 router.get("/:id", auth, userController.getUserById);
 router.put("/:id", auth, userController.updateUser);
