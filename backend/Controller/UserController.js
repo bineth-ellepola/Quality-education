aefb891271a34a94ecc577fc0bf8f7e5c4acc70d
Bineth Ellepola <binethellepola@gmail.com>
2026-02-21 13:10:34 +0530

    Updated the usermanagement model and controller

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -71,3 +71,104 @@ exports.registerUser = async (req, res) => {
     });
   }
 };
+
+//get all uders 
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
+
+//get by id
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
+
+
+//Update users
+exports.updateUser = async (req, res) => {
+  try {
+    const { first_name, last_name, email, password, role, isActive } = req.body;
+
+    const updateData = {
+      first_name,
+      last_name,
+      email,
+      role,
+      isActive
+    };
+
+    // If password updated → hash again
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      updateData.password = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      updateData,
+      { new: true, runValidators: true }
+    ).select("-password");
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
+
+
+//Delete Users 
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "User deleted successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
